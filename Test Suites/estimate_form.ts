a8a7b52d8e70a1f8836319e916d06e7fc3cbc940
Zazimko Alexey <notfoundsam@gmail.com>
2018-03-28 14:37:12 +0900

    gitignore

---
+++ Test Suites/estimate_form.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>estimate_form</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-28T14:29:44</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2637aa91-cb4a-4e2b-8038-296114a6924e</testSuiteGuid>
+   <testCaseLink>
+      <guid>5f10f41e-0cf2-4bf5-a805-5b39bd2ed5a1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/localhost/front/new_form/contact_01</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>39ae610f-a396-4a08-ba40-d8c50633cd75</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3722ce68-cd83-4395-88ce-93ec174579f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/localhost/front/new_form/contact_02</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
